c1722667d22bc65fee281392936c5a7d303c1930
henriquemattia <mattia.henrique@gmail.com>
2023-02-25 19:04:25 -0300

    Finishing the page 'AddingBalance'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { AddingBalance } from "./pages/AddingBalance/AddingBalance"
 import { QRcodeGenerated } from "./pages/QRcodeGenerated/QRcodeGenerated"
 
 function App() {
 
   return (
     <div className="App">
-      <QRcodeGenerated />
+      {/* <QRcodeGenerated /> */}
+      <AddingBalance />
     </div>
   )
 }
--- src/pages/AddingBalance/AddingBalance.tsx
+++ src/pages/AddingBalance/AddingBalance.tsx
@@ -1,7 +1,37 @@
 
+import './AddingBalance.css'
 
 export function AddingBalance() {
-    <>
-        
-    </>
+    return (
+        <>
+            <div className="container-ballance">
+
+                <p>Adicionar saldo</p>
+                <br />
+                <br />
+                <div className="payment-select">
+                    <label className="label-style">Forma de pagamento</label>
+                    <div>
+
+                        <select>
+                            <option value="">PIX</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className='value-input'>
+                    <label className="label-style">Valor</label>
+
+                    <input placeholder='R$ 1.000,00' />
+                </div>
+
+
+                <div className="buttons-area">
+                    <button className="button-back">Voltar</button>
+                    <button className="button-generate">Gerar QRCode</button>
+                </div>
+            </div>
+
+        </>
+    )
 }
