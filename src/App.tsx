b34faedc649729f7b503879959b3bba577ca7d7d
henriquemattia <mattia.henrique@gmail.com>
2023-02-26 16:49:17 -0300

    Adding responsivit on PixApproved

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { AddingBalance } from "./pages/AddingBalance/AddingBalance"
+import { Dashboard } from "./pages/Dashboard/Dashboard"
+import { PixApproved } from "./pages/PixApproved/PixApproved"
 import { QRcodeGenerated } from "./pages/QRcodeGenerated/QRcodeGenerated"
 
 function App() {
 
   return (
     <div className="App">
-      {/* <QRcodeGenerated /> */}
-      <AddingBalance />
+      <PixApproved />
     </div>
   )
 }
---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,23 @@
+
+import './Dashboard.css'
+
+export function Dashboard() {
+    return (
+        <div className="dashBoard">
+            <nav>  {/* Barra de navegação lateral esquerda  */}
+                <div>
+
+                </div>
+            </nav>
+
+            <header> {/*Cabeçalho do dashboard */}
+
+            </header>
+
+            <section className=""> {/* Secção princiapl com a tabela*/}
+
+            </section>
+
+        </div>
+    )
+}
