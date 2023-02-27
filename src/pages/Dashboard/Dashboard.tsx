649854603a34b0bff97ed85a606e47b4b750b41a
henriquemattia <mattia.henrique@gmail.com>
2023-02-26 21:39:09 -0300

    Initializing navbar of Dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,22 +1,41 @@
 
+import menuSvg from '../../assets/icon-park-outline_menu.svg'
+import houseSvg from '../../assets/house.svg'
+import moneySvg from '../../assets/money.svg'
+import logOutSvg from '../../assets/log-out.svg'
 import './Dashboard.css'
 
 export function Dashboard() {
     return (
-        <div className="dashBoard">
+        <div className="dashBoard-container">
             <nav>  {/* Barra de navegação lateral esquerda  */}
-                <div>
+                <div className='nav-bar'>
+                    <div className='nav-bar_top'>
+                        <div>
+                            <img src={menuSvg} className="menu-svg" alt="ícone de menu" />
 
+                            <img src={houseSvg} className="house-svg" alt="ícoone de uma casa" />
+                        </div>
+
+                        <span />
+                    </div>
+
+                    <div className='nav-bar_botton'>
+                            <img src={moneySvg} className="money-svg" alt="" />
+                            <img src={logOutSvg} className="logOut-svg" alt="" />
+                    </div>
                 </div>
             </nav>
 
-            <header> {/*Cabeçalho do dashboard */}
+            <main> {/*Conteudo principal da página */}
+                <header> {/*Cabeçalho do dashboard */}
+                    <p>Henader</p>
+                </header>
 
-            </header>
-
-            <section className=""> {/* Secção princiapl com a tabela*/}
-
-            </section>
+                <section className=""> {/* Secção princiapl com a tabela*/}
+                    <p>section</p>
+                </section>
+            </main>
 
         </div>
     )
