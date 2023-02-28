0b289db85e0beb17a41c98b126137fe7c4d560f4
henriquemattia <mattia.henrique@gmail.com>
2023-02-28 15:03:17 -0300

    Finishing the mid table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,3 +1,6 @@
+import copySvg from '../../assets/icon-park-outline_copy.svg'
+import codeSvg from '../../assets/icon-park-outline_pay-code-one.svg'
+import sortSvg from '../../assets/Sort.svg'
 
 import './Table.css'
 
@@ -7,105 +10,31 @@ export function Table() {
             <table>
                 <thead>
                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Destino</th>
-                        <th scope="col">Valor</th>
-                        <th scope="col">Solicitação</th>
-                        <th scope="col">Forma pgto.</th>
-                        <th scope="col">Vencimento</th>
-                        <th scope="col">Status</th>
+                        <th scope="col"><div>ID <img src={sortSvg} alt="" /></div></th>
+                        <th scope="col"><div>Destino <img src={sortSvg} alt="" /></div></th>
+                        <th scope="col"><div>Valor <img src={sortSvg} alt="" /></div></th>
+                        <th scope="col"><div>Solicitação <img src={sortSvg} alt="" /></div></th>
+                        <th scope="col"><div>Forma pgto. <img src={sortSvg} alt="" /></div></th>
+                        <th scope="col"><div>Vencimento <img src={sortSvg} alt="" /></div></th>
+                        <th scope="col"><div>Status <img src={sortSvg} alt="" /></div></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td data-label="ID">$252</td>
+                        <td data-label="ID">4145</td>
                         <td data-label="Destino">Despesa</td>
                         <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
+                        <td data-label="Solicitação" className="space">28/12/2022 23:30</td>
+                        <td data-label="Forma pgto.">PIX/Ted</td>
                         <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
-                    </tr>
-                    <tr>
-                        <td data-label="ID">$252</td>
-                        <td data-label="Destino">Despesa</td>
-                        <td data-label="Valor">R$50,00</td>
-                        <td data-label="Solicitação">28/12/2022 23:30</td>
-                        <td data-label="Forma pgto.">Pix/Ted</td>
-                        <td data-label="Vencimento">28/02/2022</td>
-                        <td data-label="Status">Em progresso</td>
+                        <td data-label="Status"><div className="badge">Em progresso</div></td>
+                        <td>
+                            <div className="icons-container">
+                                <img className="s-svg" src={copySvg} alt="" />
+                                <img className="s-svg" src={codeSvg} alt="" />
+                            </div>
+                        </td>
                     </tr>
 
                 </tbody>
