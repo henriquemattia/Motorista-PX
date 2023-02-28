da23b603cd98698e6245eb80352d2b18e383e014
henriquemattia <mattia.henrique@gmail.com>
2023-02-28 17:59:15 -0300

    Change the language

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,4 +1,4 @@
-import copySvg from '../../assets/icon-park-outline_copy.svg'
+import copySvg from '../../assets/icon-park-outline_copy-2.svg'
 import codeSvg from '../../assets/icon-park-outline_pay-code-one.svg'
 import sortSvg from '../../assets/Sort.svg'
 
